5f60093ba67fc9343d4b30f8e6ad949754b8dc50
SneakStarBerry <gihong2012@likelion.org>
2023-05-16 00:27:18 +0900

    Delete flutter directory
    delete flutter directory because not fit for subject

--- flutter/Session2nd/android/app/src/main/kotlin/com/example/Session2nd/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.Session2nd
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- flutter/session/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/sneakstarberry/session/api"
-)
-
-func main() {
-	api.Run()
-}
